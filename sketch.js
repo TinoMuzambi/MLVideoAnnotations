8cab5272c2a044814897a13f1695620a13fcfe7a
Tino <tinomuzambi@gmail.com>
2020-11-16 17:03:37 +0200

    Fixes old library problem.

--- sketch.js
+++ sketch.js
@@ -1,3 +1,70 @@
-// Your code will go here
-// open up your console - if everything loaded properly you should see 0.3.0
-console.log('ml5 version:', ml5.version);
+// Copyright (c) 2019 ml5
+//
+// This software is released under the MIT License.
+// https://opensource.org/licenses/MIT
+
+/* ===
+ml5 Example
+Webcam Image Classification using a pre-trained customized model and p5.js
+This example uses p5 preload function to create the classifier
+=== */
+
+// Classifier Variable
+let classifier;
+// Model URL
+let imageModelURL = "https://teachablemachine.withgoogle.com/models/bXy2kDNi/";
+
+// Video
+let video;
+let flippedVideo;
+// To store the classification
+let label = "";
+
+// Load the model first
+async function preload() {
+	classifier = await ml5.imageClassifier(imageModelURL + "model.json");
+}
+
+async function setup() {
+	createCanvas(320, 260);
+	// Create the video
+	video = createCapture(VIDEO);
+	video.size(320, 240);
+	video.hide();
+
+	flippedVideo = ml5.flipImage(video);
+	// Start classifying
+	await classifyVideo();
+}
+
+function draw() {
+	background(0);
+	// Draw the video
+	image(flippedVideo, 0, 0);
+
+	// Draw the label
+	fill(255);
+	textSize(16);
+	textAlign(CENTER);
+	text(label, width / 2, height - 4);
+}
+
+// Get a prediction for the current video frame
+async function classifyVideo() {
+	flippedVideo = await ml5.flipImage(video);
+	classifier.classify(flippedVideo, gotResult);
+}
+
+// When we get a result
+function gotResult(error, results) {
+	// If there is an error
+	if (error) {
+		console.error(error);
+		return;
+	}
+	// The results are in an array ordered by confidence.
+	// console.log(results[0]);
+	label = results[0].label;
+	// Classifiy again!
+	classifyVideo();
+}
